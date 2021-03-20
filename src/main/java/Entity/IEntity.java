832f0688e29b11ee7c22a812dc6ec5fcae181e54
basarYargici <ibrahimbyargici@gmail.com>
2021-03-20 03:01:59 +0300

    RESTAPI layer and City entity added. Also dev tools dependency added.

---
+++ src/main/java/Entity/IEntity.java
@@ -0,0 +1,9 @@
+package Entity;
+
+/**
+ * @author İbrahim Başar YARGICI
+ * @date 20.03.2021
+ */
+
+// if a class implements this interface, it's a table of database
+public interface IEntity {}
--- src/main/java/SolveZIP/SolveZIP.java
+++ src/main/java/SolveZIP/SolveZIP.java
@@ -1,3 +1,5 @@
+package SolveZIP;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
